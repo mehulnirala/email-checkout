f00ca1f450112941ea5f941be44f5a7961b6d69f
mehulnirala <mehulnirala@microsoft.com>
2022-05-12 21:41:28 +0530

    Added build.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import { createCheckout, getPaymentMethods, getCustomersList } from './checkout';
 
 dotenv.config();
@@ -7,19 +8,17 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT||3000;
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.get('/', (req: Request, res: Response) => {
-  // console.log(req);
-  // res.sendFile('public/index.html', {root: __dirname })
-  // getPaymentMethods({country: 'IN', currency: 'INR'}).then(data => res.send(JSON.stringify(data, null, 2)));
-  createCheckout({ amount: 1, country: 'IN', currency: 'INR', customerId: 'customerId'}).then(data => res.send(data));
-  // getCustomersList().then((data)=>res.send(data));
-});
-
-
-app.post('/', (req: Request, res: Response) => {
-  console.log(req);
+//   // console.log(req);
+//   // res.sendFile('public/index.html', {root: __dirname })
+//   // getPaymentMethods({country: 'IN', currency: 'INR'}).then(data => res.send(JSON.stringify(data, null, 2)));
+//   // createCheckout({ amount: 1, country: 'IN', currency: 'INR', customerId: 'customerId'}).then(data => res.send(data));
+//   // getCustomersList().then((data)=>res.send(data));
   res.send('done!')
 });
+
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
